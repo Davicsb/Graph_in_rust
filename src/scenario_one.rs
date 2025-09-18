840aba28d17aa87b33bd34b53540019a11473d9d
Davi Celestino <davi.csb@gmail.com>
2025-09-18 14:55:38 -0300

    Scenario 1

--- src/scenario_one.rs
+++ src/scenario_one.rs
@@ -1,10 +1,10 @@
 use crate::graph::read_graph;
 use crate::floydwarshall::floyd;
 
-fn central_estation(matriz : Vec<Vec<i32>>, num_v : usize) -> usize{
+fn def_central_station(matriz : &Vec<Vec<i32>>, num_v : usize) -> usize{
     let inf = i32::MAX/2;
     let mut lowest_sum = i32::MAX;
-    let mut c_estation = 0;
+    let mut c_station = 0;
     for i in 0..num_v{
         let mut sum = 0;
         for j in 0..num_v{
@@ -16,14 +16,32 @@ fn central_estation(matriz : Vec<Vec<i32>>, num_v : usize) -> usize{
         }
         if sum < lowest_sum{
             lowest_sum = sum;
-            c_estation = i;
+            c_station = i;
         }
     }
 
-    c_estation+1
+    c_station+1
 }
 
-fn print_matriz(matriz : Vec<Vec<i32>>, num_v : usize){
+fn station_vector(matriz: &[Vec<i32>], station: usize, num_v: usize) -> (Vec<i32>, usize, i32) {
+    let mut ev: Vec<i32> = Vec::with_capacity(num_v); 
+    
+    let mut farthest_station = 0;
+    let mut farthest_distance = i32::MIN;
+    
+    for i in 0..num_v {
+        let distancia_atual = matriz[station - 1][i];
+        ev.push(distancia_atual); 
+        if distancia_atual > farthest_distance {
+            farthest_distance = distancia_atual;
+            farthest_station = i + 1;
+        }
+    }
+
+    (ev, farthest_station, farthest_distance)
+}
+
+fn print_matriz(matriz : &Vec<Vec<i32>>, num_v : usize){
     let inf = i32::MAX/2;
     for i in 0..num_v{
         print!("{} -> ", i + 1);
@@ -32,11 +50,22 @@ fn print_matriz(matriz : Vec<Vec<i32>>, num_v : usize){
                 print!("{} ", matriz[i][j]);
             }
             else{
-                print!("inf ");
+                print!("# ");
             }
         }
         print!("\n");
     }
+}
+
+fn print_vector(vector : &Vec<i32>){
+    print!("[");
+    for i in 0..vector.len(){
+        print!("{}", vector[i]);
+        if i < vector.len()-1{
+            print!(", ");
+        }
+    }
+    print!("]\n");
 }
 
 pub fn first_scenario(){
@@ -53,10 +82,20 @@ pub fn first_scenario(){
 
     let matriz = floyd(&gr);
     let num_v = gr.num_vertex();
+    let central_station = def_central_station(&matriz, num_v);
+    let (central_station_vector, farthest_station, farthest_distance) = station_vector(&matriz, central_station, num_v);
 
-    print!("A estação central é: {}\n", central_estation(matriz.clone(), num_v));
+
+    print!("O nó que representa a estação central escolhida: {}\n", central_station);
     print!("\n");
-    print_matriz(matriz, num_v);
+    print!("Um vetor com as distâncias da estação central até os demais vértices:");
+    print_vector(&central_station_vector);
+    print!("\n");
+    print!("O vértice mais distante da estação central, junto com o valor de distância: {}, d = {}\n", farthest_station, farthest_distance);
+    print!("\n");
+    print!("Matriz em que cada linha representa um vértice candidato à estação central e cada coluna é a distância mínima entre o vértice candidato e o vértice representante da coluna:\n");
+    print_matriz(&matriz, num_v);
+    print!("\n");
     
 
 }
