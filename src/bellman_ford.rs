6f4e5654adca3ee69d7825887adfe7f894cf41e6
Bahamut <jvtv@ic.ufal.br>
2025-09-19 04:05:12 -0300

    adicionado uma checagem de ciclo negativo

--- src/bellman_ford.rs
+++ src/bellman_ford.rs
@@ -1,17 +1,18 @@
 use crate::graph::Graph;
 
 pub fn bellman_ford(grafo: &Graph, origem: &usize) -> (Vec<i32>, Vec<Option<usize>>) { //retorna as distancias e anteriores pros caminhos
-    let num_v = grafo.num_vertex() + 1;
+    let num_v = grafo.num_vertex();
 
     // Linha 1: Cria um vetor de distâncias, inicializando todos com INFINITO. Em seguida a distancia com a origem recebe zero
     //é criado tambem o vetor anterior
-    let mut d = vec![i32::MAX/2; num_v];
+    let mut d = vec![i32::MAX/2; num_v + 1];
     d[*origem] = 0;
-    let mut anterior = vec![None; num_v];
+    let mut anterior = vec![None; num_v + 1];
 
     //Linha 2: Inicio do loop, enquanto existir uma aresta (j,i) no grafo tal que d[i] > d[j] + vij (peso) fazer
     let mut mudou = true;
-    while mudou {
+    let mut iteracoes = 0;
+    while mudou && (iteracoes < num_v) {
         mudou = false;
 
         for(j, lista_vizinhos) in &grafo.adj {
@@ -22,6 +23,11 @@ pub fn bellman_ford(grafo: &Graph, origem: &usize) -> (Vec<i32>, Vec<Option<usiz
                 let v = node.weight;
 
                 if d[i] > d[*j] + v{
+                    //Checagem pra ver se o grafo tem ciclo negativo
+                    if(iteracoes == num_v - 1){ //Na iteração V (lembre que iteracoes começou com 0), se houver outra mudança é porque o grafo tem ciclo negativo
+                        panic!("O grafo tem ciclo negativo, o algoritmo de bellman-ford não pode ser utilizado.");
+                    }
+
                     //Linha 3: d[i] recebe d[j] + vij e anterior[i] recebe j
                     d[i] = d[*j] + v;
                     anterior[i] = Some(*j);
@@ -31,6 +37,7 @@ pub fn bellman_ford(grafo: &Graph, origem: &usize) -> (Vec<i32>, Vec<Option<usiz
                 head = node.next.as_ref();
             }
         }
+        iteracoes += 1;
     }
 
     (d, anterior)
