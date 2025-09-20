55062bfc7e71ae58c0addbd460edec297adeae62
Davi Celestino <davi.csb@gmail.com>
2025-09-20 14:40:56 -0300

    Third scenario

--- src/scenario_three.rs
+++ src/scenario_three.rs
@@ -5,9 +5,6 @@ pub fn third_scenario(){
     if let Ok((matrix, start, goal)) = read_map("data/grid_example.txt") {
         println!("Mapa lido com sucesso!");
         println!("Início: {:?}, Fim: {:?}", start, goal);
-        
-        // Mova a chamada para DENTRO do bloco
-        // E passe uma referência com `&`
         if let Err(e) = map_to_txt(&matrix) {
             eprintln!("Erro ao escrever o arquivo de grafo: {}", e);
         }
