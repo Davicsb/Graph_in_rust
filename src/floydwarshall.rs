f545a155996049a0cf5ddc030e724d77819ae252
Bahamut <jvtv@ic.ufal.br>
2025-09-21 00:58:42 -0300

    escolha de casos e funções que provavelmente não vão ser usadas

--- src/floydwarshall.rs
+++ src/floydwarshall.rs
@@ -1,4 +1,20 @@
-use crate::graph::Graph; 
+use crate::graph::Graph;
+
+pub fn reconstruir_caminho(rot: &Vec<Vec<Option<usize>>>, origem: usize, destino: usize) -> Vec<usize> {
+    let mut caminho = vec![origem];
+    let mut head = origem;
+
+    while head != destino {
+        if let Some(next) = rot[head][destino] {
+            head = next;
+            caminho.push(head);
+        } else {
+            return vec![];
+        }
+    }
+
+    caminho
+}
 
 pub fn floyd(gr : &Graph) -> Vec<Vec<i32>>{
 
@@ -44,4 +60,50 @@ pub fn floyd(gr : &Graph) -> Vec<Vec<i32>>{
     }
 
     cost
+}
+
+pub fn floyd_rot_n_cost(gr : &Graph) -> (Vec<Vec<i32>>, Vec<Vec<Option<usize>>>){ //retorna as duas matrizes
+
+    let inf = i32::MAX/2;
+    let num_v = gr.num_vertex();
+    let mut rot = vec![vec![None; num_v]; num_v]; // Matriz de roteamento Rij
+    let mut cost = vec![vec![inf; num_v]; num_v]; // matriz de custo D^0
+
+    for i in 0..num_v{
+        rot[i][i] = Some(i); // Caminho dele para ele mesmo é ele
+        cost[i][i] = 0; // Custo do vétice para ele mesmo é 0
+    }
+
+    for (origin, adj_list) in &gr.adj // Pega o par de origem e os nós ligados a origem
+    {
+        if let Some(mut node) = adj_list.as_deref() // Pega a lista de nós
+        {
+            loop{
+                rot[*origin][node.value] = Some(node.value); // Rij <- j
+                cost[*origin][node.value] = node.weight; // Dij <- V(G)
+
+                if let Some(next_node) = node.next.as_deref(){
+                    node = next_node; // Chama próximo nó
+                }
+                
+                else{
+                    break; // Não existe mais nós
+                }
+            }
+        }
+    }
+
+    for k in 0..num_v{ // Para k = 0 ... n
+        for i in 0..num_v{ // Para i = 0 ... n
+            for j in 0..num_v{ // Para j = 0 ... n
+                if (cost[i][k] + cost[k][j]) < cost[i][j] // if Dik + Dkj < Dij
+                {
+                    cost[i][j] = cost[i][k] + cost[k][j]; // Dij <- Dik + Dkj
+                    rot[i][j] = rot[i][k]; // Rij <- Rik
+                }
+            }
+        }
+    }
+
+    (cost, rot)
 }
--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,23 @@ mod scenario_one;
 mod scenario_two;
 mod scenario_three;
 
+use std::io;
 use crate::scenario_one::first_scenario;
 use crate::scenario_two::second_scenario;
 use crate::scenario_three::third_scenario;
 
 fn main() {
-    first_scenario();
-    //second_scenario();
-    third_scenario();
+    println!("Escolha um cenário:\n1 - Determinando a estação central\n2 - Otimizando caminho com regeneração\n3 - Robô de armazém com obstáculos");
+    
+    let mut input_line = String::new();
+    io::stdin().read_line(&mut input_line).expect("Falha ao ler a linha.");
+    let num : i32 = input_line.trim().parse().expect("O input não é um inteiro");
+    //tudo isso pra ler um int...
+
+    match num {
+        1 => first_scenario(),
+        2 => second_scenario(),
+        3 => third_scenario(),
+        _ => println!("Cenário inválido.")
+    };
 }
